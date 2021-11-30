9f62537db29364f8dd0b66e151175cef9d84ad33
MatthieuChabot <chbt.matthieu@gmail.com>
2021-11-30 12:16:21 +0100

    finsihing quest n 9

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Aventure',
+        'Animation',
+        'Fantastique',
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
